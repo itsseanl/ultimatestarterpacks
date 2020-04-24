6094ee56e96178d0ac09c17dc7cceb3127cd8ac9
Sean Lyons <seanlyons@Seans-MBP.lan>
2020-04-24 11:35:36 -0400

    small tweaks

--- pages/api/pack/[slug].js
+++ pages/api/pack/[slug].js
@@ -5,11 +5,13 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
+	//get slug from requesting page
 	const {
 		query: { slug },
 	} = req;
 	console.log("query: " + slug);
 	console.log("request: " + req);
+	//get single item and return
 	let doc = await req.db.collection("packs").find({ title: slug }).toArray();
 	res.json(doc);
 });
